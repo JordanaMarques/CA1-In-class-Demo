359f44d2f0fc0f79a54ef78855ca5327252a7363
JordanaMarques <jojoo.makes@gmail.com>
2019-12-06 15:35:42 +0000

    Delete option3

--- views/jquery/table.js
+++ views/jquery/table.js
@@ -1,54 +1,55 @@
 function draw_table()
 {
-    $("#results").empty();
-    $.getJSONuncached = function(url) 
-    {
-        return $.ajax(
-        {
-            url: url,
-            type: 'GET',
-            cache: false,
-            success: function(html) 
-            {
-                $("#results").append(html);
-                select_row();
-            }
-        });
-    };
-    $.getJSONuncached("/get/html")
+	$("#results").empty();
+	$.getJSONuncached = function (url)
+	{
+		return $.ajax(
+		{
+			url: url,
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#results").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/get/html")
 };
+
 function select_row()
 {
-    $("#menuTable tbody tr[id]").click(function ()
-    {
-         
-        $(".selected").removeClass("selected");
-        $(this).addClass("selected");
-        var section = $(this).prevAll("tr").children("td[colspan=’4’ ").length   -1;
-        var entrée = $(this).attr("id") -1;
-        delete_row(section, entree);
-    })
+	$("#menuTable tbody tr[id]").click(function ()
+	{
+		$(".selected").removeClass("selected");
+		$(this).addClass("selected");
+		var section = $(this).prevAll("tr").children("td[colspan='4']").length - 1;
+		var entree = $(this).attr("id") - 1;
+		delete_row(section, entree);
+	})
 };
 
 function delete_row(sec, ent)
 {
-    $("#delete").click(function ()
-    {
-        $.ajax(
-        {
-            url:"/post/delete",
-            type:"POST",
-            data:
-            {
-                 Section: sec,
-                 entree: ent
-            },
-            cache: false,
-            success: setTimeout(draw_table, 1000)
-         })
-    })
+	$("#delete").click(function ()
+	{
+		$.ajax(
+		{
+			url: "/post/delete",
+			type: "POST",
+			data:
+			{
+				section: sec,
+				entree: ent
+			},
+			cache: false,
+			success: setTimeout(draw_table, 1000)
+		})
+	})
 };
+
 $(document).ready(function ()
 {
-    draw_table();
+	draw_table();
 });
