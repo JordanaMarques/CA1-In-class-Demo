be56adc9c6073291181b2471ad6a5578e0b8cc26
JordanaMarques <jojoo.makes@gmail.com>
2020-04-27 15:37:46 +0530

    add models and public scripts

---
+++ node-rest-api/app/models/SecretEscapes.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const secretSchema = new Schema({
+	service: {
+		type: String,
+		required: [ true, 'Service is required' ]
+	},
+	item: {
+		type: String,
+		required: [ true, 'Destination  is required' ]
+	},
+	price: {
+		type: String,
+		required: [ true, 'Price  is required' ]
+	},
+	day: {
+		type: String,
+		required: [ true, 'Day  is required' ]
+	},
+	createdAt: {
+		type: Date,
+		default: new Date()
+	}
+});
+
+const Sescapes = mongoose.model('sescapes', secretSchema);
+export default Sescapes;
